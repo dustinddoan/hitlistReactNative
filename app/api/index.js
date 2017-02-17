7ec62551fd92fddbd43a61ceb5904553710bdd45
Dustin Doan <dustin.ddoan@gmail.com>
2017-02-17 00:15:09 -0800

    add API URL

--- app/api/index.js
+++ app/api/index.js
@@ -1,6 +1,4 @@
-var API_URL = 'http://localhost:3000/v1'
+// var API_URL = 'http://localhost:3000/v1'
+var API_URL = 'https://hidden-plains-11034.herokuapp.com/'
 exports.SIGNIN_URL = `${API_URL}/signin`
 exports.SIGNUP_URL = `${API_URL}/signup`
-
-
-// var API_URL = 'https://fathomless-reef-57802.herokuapp.com/v1';
