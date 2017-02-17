d6ffb9d26d88507c65169c0b6552f4bfdea5a7a0
Dustin Doan <dustin.ddoan@gmail.com>
2017-02-17 00:16:55 -0800

    update API URL

--- app/api/index.js
+++ app/api/index.js
@@ -1,4 +1,4 @@
 // var API_URL = 'http://localhost:3000/v1'
-var API_URL = 'https://hidden-plains-11034.herokuapp.com/'
+var API_URL = 'https://hidden-plains-11034.herokuapp.com/v1'
 exports.SIGNIN_URL = `${API_URL}/signin`
 exports.SIGNUP_URL = `${API_URL}/signup`
