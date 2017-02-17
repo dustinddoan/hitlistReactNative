faa9083e38718b10d298c23d4f9249659c04e1de
Dustin Doan <dustin.ddoan@gmail.com>
2017-02-17 00:22:01 -0800

    update signup withou keychain

--- app/actions/authActions.js
+++ app/actions/authActions.js
@@ -22,22 +22,38 @@ exports.loginUser = (email, password) => {
       })
   }
 }
-
 exports.signupUser = (email, password) => {
   return function(dispatch) {
-    return axios.post(SIGNUP_URL, {email, password}).then((response) => {
-      var {user_id, token} = response.data;
-      Keychain.setGenericPassword(user_id, token)
-        .then(function() {
-          dispatch(authUser(user_id, token));
-        }).catch((error) => {
-          dispatch(addAlert("Could not log in."));
-        });
-    }).catch((error) => {
-      dispatch(addAlert("Could not sign up."));
-    });
+    return axios.post(SIGNUP_URL, {email, password})
+      .then((response) => {
+        return new Promise((resolve, reject) => {
+          var {user_id, token, user} = response.data
+          console.log("Response from ")
+          dispatch(authUser(user_id, token, user))
+          // dispatch(addAlert(token))
+          resolve({user_id, token, user})
+        })
+      }).catch((error) => {
+        dispatch(addAlert('Could not sign up'))
+      })
   }
 }
+
+// exports.signupUser = (email, password) => {
+//   return function(dispatch) {
+//     return axios.post(SIGNUP_URL, {email, password}).then((response) => {
+//       var {user_id, token} = response.data;
+//       Keychain.setGenericPassword(user_id, token)
+//         .then(function() {
+//           dispatch(authUser(user_id, token));
+//         }).catch((error) => {
+//           dispatch(addAlert("Could not log in."));
+//         });
+//     }).catch((error) => {
+//       dispatch(addAlert("Could not sign up."));
+//     });
+//   }
+// }
 
 authUser = (user_id, token, currentUser) => {
   return {
